08b8f83811c2dbf076b0ba1aa9f089d95e6cbc81
Mymoza <marie-philippe.gill@hotmail.ca>
2016-12-03 21:16:50 -0500

    Day 2 Part 1 Almost

--- days/day2.py
+++ days/day2.py
@@ -7,26 +7,27 @@ file_day2 = open("day2", "r")
 
 class bathroomDoor:
 
-    keys = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+    keys = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+            ]
     x = y = 1
+    numbers = []
 
     def find_number(self, line):
         action = 0
         for letter in str(line): # Explore Letter by Letter until the end
             if letter == "U":
-                action = -3
+                self.y = max(0, self.y - 1)
             elif letter == "D":
-                action = 3
+                self.y = min(2, self.y + 1)
             elif letter == "L":
-                action = -1
+                self.x = max(0, self.x - 1)
             elif letter == "R":
-                action = 1
-
-            if (self.current_pos + action) > 0 and (self.current_pos + action) < 10:
-                self.current_pos += action
-
-        print "current pos", self.current_pos
-        return self.current_pos
+                self.x = min(2, self.x + 1)
+        print self.y, self.x
+        return self.y, self.x
 
 
     def readfile(self, file):
@@ -34,7 +35,8 @@ class bathroomDoor:
         result = ""
         for line in c: # Explore line by line
             result += str(self.find_number(c))
-        return result
+            self.numbers.append(str(self.keys[self.y][self.x]))
+        return self.numbers
 
 
 
